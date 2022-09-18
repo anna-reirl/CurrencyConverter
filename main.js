5cb6ceb5f3aeb73e8a01bf6664c5c7fc8a3bf68b
anna_reirl <anna.reil.student@gmail.com>
2022-09-18 17:43:01 +0400

    feat: start project

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello world!');
+fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
